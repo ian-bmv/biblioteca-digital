53a804fc8908f19394e5da2d95978a21a1aa1db9
ian-bmv <ianmorelosb@gmail.com>
2025-06-17 19:58:56 -0600

    Update books.js

--- books.js
+++ books.js
@@ -23,57 +23,57 @@ const books = [
     pdf: "https://drive.google.com/file/d/17lyEaU5XdbTZjC9pDrkmOKKo0A6ONaRz/view?usp=drive_link"
   },
   {
-    tittle: "El Principito",
+    title: "El Principito",
     author: "Antoine de Saint-Exupéry",
     pdf: "https://drive.google.com/file/d/1_D7jANQuhwbr9KdSFwh0Yy7d_IwljXbL/preview"
   },
   {
-    tittle: "Ahora te Toca ser Feliz",
+    title: "Ahora te Toca ser Feliz",
     author: "Curro Carnete",
     pdf: "https://drive.google.com/file/d/1ZokRqEu6hZYFcnAX4F9CocCeAB1bGV03/view?usp=drive_link"
   },
   {
-    tittle: "Si lo Crees, lo Creas",
+    title: "Si lo Crees, lo Creas",
     author: "Brian Tracy",
     pdf: "https://drive.google.com/file/d/1CDhe5bKGI3fMcdTj95qJHZRNmqrZOSwU/view?usp=drive_link"
   },
   {
-    tittle: "Donde tus sueños te lleven",
+    title: "Donde tus sueños te lleven",
     author: "Javier Iriondo Narvaiza",
     pdf: "https://drive.google.com/file/d/1rIzVaRSa8mdwon-MTfhjEc9-7LQrrAWS/view?usp=drive_link"
   },
   {
-    tittle: "El monje que vendio su ferrari",
+    title: "El monje que vendio su ferrari",
     author: "Sharma, Robin S.",
     pdf: "https://drive.google.com/file/d/1mGXHH_EpvhEmUcryeCxHo45ePlaNDcKN/view?usp=drive_link"
   },
   {
-    tittle: "El peligro de estar cuerda",
+    title: "El peligro de estar cuerda",
     author: "Rosa Montero",
     pdf: "https://drive.google.com/file/d/1vBVtQuBI5MoPIquekR3Vo4qSqmOWIlFG/view?usp=drive_link"
   },
   {
-    tittle: "El poder de confiaar en ti",
+    title: "El poder de confiaar en ti",
     author: "Curro Carrete",
     pdf: "https://drive.google.com/file/d/1Y9baljg4RNrJ-tPk09pxWvZd2eUbm_JE/view?usp=drive_link"
   },
   {
-    tittle: "El poder de las palabras",
+    title: "El poder de las palabras",
     author: "Mariano Sigman",
     pdf: "https://drive.google.com/file/d/1oOtG_eEVcDGJhR4GuYI5DIPlc5g9aUa1/view?usp=drive_link"
   },
   {
-    tittle: "Respira",
+    title: "Respira",
     author: "James nestor",
     pdf: "https://drive.google.com/file/d/14aJYsgAvQypqOoiZkzzKDLllh2HToT8m/view?usp=drive_link"
   },
   {
-    tittle: "La vida te esta espeando",
+    title: "La vida te esta espeando",
     author: "Javier Iriondo",
     pdf: "https://drive.google.com/file/d/1KU9CLty_DClSQAi--1zgpkOGeHc9BLNS/view?usp=drive_link"
   },
   {
-    tittle: "Limpieza, orden y felicidad",
+    title: "Limpieza, orden y felicidad",
     author: "Bego la Ordenatriz",
     pdf: "https://drive.google.com/file/d/1DDG5P9S4rT4ZYOOSDmBvbNgwHXPQYC1p/view?usp=drive_link"
   },
@@ -83,62 +83,62 @@ const books = [
     pdf: "https://drive.google.com/file/d/1rosMOgVQe3qd1SWI9qrkj9XDF7GB_I4P/view?usp=drive_link"
   },
   {
-    tittle: "Como hacer que te pasen cosas buenas",
+    title: "Como hacer que te pasen cosas buenas",
     author: "Marian Rojas",
     pdf: "https://drive.google.com/file/d/1r5RgwcFYDzuYnOdphX-NqOJ32FiM6orA/view?usp=drive_link"
   },
   {
-    tittle: "Encuentra tu persona vitamina",
+    title: "Encuentra tu persona vitamina",
     author: "Marian rojas",
     pdf: "https://drive.google.com/file/d/1oI3r-4oPAQnIkBmKwa6XUxlUOMNqmkCF/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) Sapiens. De animales a dioses",
+    title: "(AUDIO-LIBRO) Sapiens. De animales a dioses",
     author: "Yuval Noah",
     audio: "https://drive.google.com/file/d/1BdJqXE5K-5UbWjluhZY1ouLO9D9KSATW/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) Habitos Atomicos",
+    title: "(AUDIO-LIBRO) Habitos Atomicos",
     author: "James Clear",
     audio: "https://drive.google.com/file/d/1NjkiCMBRjyQEsd2E5ml0hgO2wpK4-D1w/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) El sutil arte de que te importe un carajo",
+    title: "(AUDIO-LIBRO) El sutil arte de que te importe un carajo",
     author: "Mark Manson",
     audio: "https://drive.google.com/file/d/17-FXxmwHOt2A9O2wAKNdg4P91zcA7pcr/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) El alquimista",
+    title: "(AUDIO-LIBRO) El alquimista",
     author: "Paulo Coelho",
     audio: "https://drive.google.com/file/d/1SSzRXyPq9Y7fsUYT7P6gWxRmaBPNrNvC/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) Piense y hagase rico",
+    title: "(AUDIO-LIBRO) Piense y hagase rico",
     author: "Napoleon Hill",
     audio: "https://drive.google.com/file/d/1DPqNIv_r099l22KaO-ycdlBhRDE7Zzgj/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) Tus zonas erroneas",
+    title: "(AUDIO-LIBRO) Tus zonas erroneas",
     author: "Wayne",
     audio: "https://drive.google.com/file/d/1ntvaNgwQxuktBROnxjqyS8BboM4zkCXY/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) El club de las 5 de la mañana",
+    title: "(AUDIO-LIBRO) El club de las 5 de la mañana",
     author: "Robin Sharma",
     audio: "https://drive.google.com/file/d/19-1DyUITpcJ5vyHzRd7iNiipG14U2ybj/view?usp=drive_link"
   },
   {
-    tittle:  "(AUDIO-LIBRO) El hombre en busca del sentido",
+    title:  "(AUDIO-LIBRO) El hombre en busca del sentido",
     author: "Viktor Frank",
     audio: "https://drive.google.com/file/d/1rvLLq_koPzOqFU7N3FFyR_MLbh8vFIrc/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) Pequeño cerdo capitalista",
+    title: "(AUDIO-LIBRO) Pequeño cerdo capitalista",
     author: "Sofia Maciasl",
     audio: "https://drive.google.com/file/d/13-AbYb5boLXPKeP2onMM0DCI1OrtCx50/view?usp=drive_link"
   },
   {
-    tittle: "(AUDIO-LIBRO) Como ganar amigos e influir",
+    title: "(AUDIO-LIBRO) Como ganar amigos e influir",
     author: "Dale Carnegie",
     audio: "https://drive.google.com/file/d/1hP9UPdqi-mTtns0bgCl_97X1kuhcjecY/view?usp=drive_link"
   }

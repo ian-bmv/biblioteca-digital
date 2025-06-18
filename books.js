bcd4a583ebc2220a7ff8f7a2650edc7a447bedec
ian-bmv <ianmorelosb@gmail.com>
2025-06-17 19:19:05 -0600

    Update books.js

--- books.js
+++ books.js
@@ -21,5 +21,75 @@ const books = [
     title: "Agua y Jabon",
     author: "Marta D. Riezu",
     pdf: "https://drive.google.com/file/d/17lyEaU5XdbTZjC9pDrkmOKKo0A6ONaRz/view?usp=drive_link"
+  },
+  {
+    tittle: "El Principito",
+    author: "Antoine de Saint-Exupéry",
+    pdf: "https://drive.google.com/file/d/1_D7jANQuhwbr9KdSFwh0Yy7d_IwljXbL/preview"
+  },
+  {
+    tittle: "Ahora te Toca ser Feliz",
+    author: "Curro Carnete",
+    pdf: "https://drive.google.com/file/d/1ZokRqEu6hZYFcnAX4F9CocCeAB1bGV03/view?usp=drive_link"
+  },
+  {
+    tittle: "Si lo Crees, lo Creas",
+    author: "Brian Tracy",
+    pdf: "https://drive.google.com/file/d/1CDhe5bKGI3fMcdTj95qJHZRNmqrZOSwU/view?usp=drive_link"
+  },
+  {
+    tittle: "Donde tus sueños te lleven",
+    author: "Javier Iriondo Narvaiza",
+    pdf: "https://drive.google.com/file/d/1rIzVaRSa8mdwon-MTfhjEc9-7LQrrAWS/view?usp=drive_link"
+  },
+  {
+    tittle: "El monje que vendio su ferrari",
+    author: "Sharma, Robin S.",
+    pdf: "https://drive.google.com/file/d/1mGXHH_EpvhEmUcryeCxHo45ePlaNDcKN/view?usp=drive_link"
+  },
+  {
+    tittle: "El peligro de estar cuerda",
+    author: "Rosa Montero",
+    pdf: "https://drive.google.com/file/d/1vBVtQuBI5MoPIquekR3Vo4qSqmOWIlFG/view?usp=drive_link"
+  },
+  {
+    tittle: "El poder de confiaar en ti",
+    author: "Curro Carrete",
+    pdf: "https://drive.google.com/file/d/1Y9baljg4RNrJ-tPk09pxWvZd2eUbm_JE/view?usp=drive_link"
+  },
+  {
+    tittle: "El poder de las palabras",
+    author: "Mariano Sigman",
+    pdf: "https://drive.google.com/file/d/1oOtG_eEVcDGJhR4GuYI5DIPlc5g9aUa1/view?usp=drive_link"
+  },
+  {
+    tittle: "Respira",
+    author: "James nestor",
+    pdf: "https://drive.google.com/file/d/14aJYsgAvQypqOoiZkzzKDLllh2HToT8m/view?usp=drive_link"
+  },
+  {
+    tittle: "La vida te esta espeando",
+    author: "Javier Iriondo",
+    pdf: "https://drive.google.com/file/d/1KU9CLty_DClSQAi--1zgpkOGeHc9BLNS/view?usp=drive_link"
+  },
+  {
+    tittle: "Limpieza, orden y felicidad",
+    author: "Bego la Ordenatriz",
+    pdf: "https://drive.google.com/file/d/1DDG5P9S4rT4ZYOOSDmBvbNgwHXPQYC1p/view?usp=drive_link"
+  },
+  {
+    tittle: "Lo bueno de tener un mal dia",
+    author: "Anabel Gonzales",
+    pdf: "https://drive.google.com/file/d/1rosMOgVQe3qd1SWI9qrkj9XDF7GB_I4P/view?usp=drive_link"
+  },
+  {
+    tittle: "Como hacer que te pasen cosas buenas",
+    author: "Marian Rojas",
+    pdf: "https://drive.google.com/file/d/1r5RgwcFYDzuYnOdphX-NqOJ32FiM6orA/view?usp=drive_link"
+  },
+  {
+    tittle: "Encuentra tu persona vitamina",
+    author: "Marian rojas",
+    pdf: "https://drive.google.com/file/d/1oI3r-4oPAQnIkBmKwa6XUxlUOMNqmkCF/view?usp=drive_link"
   }
   ];
